832df620887f6ab89f271fe8d0c87a82b94ef143
CaoTrongThang <51023235+CaoTrongThang@users.noreply.github.com>
2024-11-29 15:58:54 +0700

    add damage mutiply for arrows and explosions

--- fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/mixin/LivingEntityMixin.java
+++ fabric/src/main/java/io/github/flemmli97/improvedmobs/fabric/mixin/LivingEntityMixin.java
@@ -1,6 +1,7 @@
 package io.github.flemmli97.improvedmobs.fabric.mixin;
 
 import io.github.flemmli97.improvedmobs.events.EventCalls;
+import io.github.flemmli97.improvedmobs.utils.EntityFlags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,6 +23,22 @@ public abstract class LivingEntityMixin {
         if (EventCalls.onAttackEvent((LivingEntity) (Object) this, source)) {
             info.setReturnValue(false);
             info.cancel();
+        }
+    }
+
+    @Inject(method = "damage", at = @At("HEAD"), cancellable = true)
+    private void onDamage(DamageSource source, float damageAmount, CallbackInfoReturnable<Boolean> cir) {
+
+        LivingEntity entity = (LivingEntity) (Object) this;
+
+        if (source.getSource() instanceof ArrowEntity) {
+            //Increase damage dealt by Arrows
+            entity.damage(entity.getWorld().getDamageSources().generic(), damageAmount * EntityFlags.projMult);
+        }
+
+        if ("explosion.player".equals(source.getName())) {
+            //Increase damage dealt by Explosions
+            entity.damage(entity.getWorld().getDamageSources().generic(), damageAmount * EntityFlags.explosionMult);
         }
     }
 }
